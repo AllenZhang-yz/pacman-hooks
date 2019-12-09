2b58372ace20f03d28b2ab1df0b6f62f4f7ec64d
Allen Zhang <allen.zhang018@gmail.com>
2019-12-10 10:55:49 +1100

    add hooks to manage state

--- src/components/Simulator.js
+++ src/components/Simulator.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import InputArea from "./InputArea";
 import PacmanMap from "./PacmanMap";
 
@@ -21,80 +21,74 @@ const initialState = {
   message: ""
 };
 
-class Simulator extends React.Component {
-  constructor() {
-    super();
-    this.state = initialState;
-  }
-
+const Simulator = () => {
+  const [isReport, setIsReport] = useState(initialState.isReport);
+  const [gridNumber, setGridNumber] = useState(initialState.gridNumber);
+  const [direction, setDirection] = useState(initialState.direction);
+  const [position, setPosition] = useState(initialState.position);
+  const [inputCommand, setInputCommand] = useState(initialState.inputCommand);
+  const [message, setMessage] = useState(initialState.message);
   // set input value when onChange
-  handleChange = value => {
-    this.setState({
-      inputCommand: value
-    });
+  const handleChange = value => {
+    setInputCommand(value);
   };
 
   // reset initialState when click RESET Button
-  handleClick = () => {
-    this.setState(initialState);
+  const handleClick = () => {
+    setIsReport(initialState.isReport);
+    setGridNumber(initialState.gridNumber);
+    setDirection(initialState.direction);
+    setPosition(initialState.position);
+    setInputCommand(initialState.inputCommand);
+    setMessage(initialState.message);
   };
 
   // press 'enter' on keyboard
-  onInputSubmit = e => {
+  const onInputSubmit = e => {
     e.preventDefault();
-    const { inputCommand, position, isReport } = this.state;
     // reset message state and
-    this.setState({
-      message: ""
-    });
+    setMessage("");
 
     if (isReport) {
-      this.setState({
-        message: "Please click RESET button to start a new test!"
-      });
+      setMessage("Please click RESET button to start a new test!");
       return;
     }
 
     // eslint-disable-next-line
     let commands = inputCommand.toUpperCase().split(/\s|\,/);
 
-    if (!this.availablePosition(position) && commands[0] !== "PLACE") {
+    if (!availablePosition(position) && commands[0] !== "PLACE") {
       // ErrorMsg: invalid command
-      this.setState({
-        message: "Please enter place command FIRST!"
-      });
+      setMessage("Please enter place command FIRST!");
       return;
     }
     if (commands[0] === "PLACE" && commands.length === 4) {
-      this.placePacman(commands.slice(1));
+      placePacman(commands.slice(1));
       return;
     }
     if (commands[0] === "MOVE") {
-      this.movePacman();
+      movePacman();
       return;
     }
     if (commands[0] === "LEFT" || commands[0] === "RIGHT") {
-      this.rotatePacman(commands[0]);
+      rotatePacman(commands[0]);
       return;
     }
     if (commands[0] === "REPORT") {
-      this.report();
+      report();
       return;
     }
 
     // ErrorMsg: invalid command
-    this.setState({
-      message: "Invalid Command!"
-    });
+    setMessage("Invalid Command!");
   };
 
-  getIndexFromArray = (value, arr) => {
+  const getIndexFromArray = (value, arr) => {
     return arr.indexOf(value);
   };
 
   // check is position in map
-  availablePosition = position => {
-    const { gridNumber } = this.state;
+  const availablePosition = position => {
     if (position.x < 0 || position.x >= gridNumber) {
       return false;
     }
@@ -106,63 +100,46 @@ class Simulator extends React.Component {
   };
 
   // place command
-  placePacman = ([x, y, face]) => {
-    const { gridNumber } = this.state;
+  const placePacman = ([x, y, face]) => {
     x = x - 0;
     y = y - 0;
-    if (!this.availablePosition({ x, y })) {
+    if (!availablePosition({ x, y })) {
       // ErrorMsg: invalid place
-      this.setState({
-        message: `Invalid Position, Please place position X and Y in 0 ~ ${gridNumber -
+      setMessage(
+        `Invalid Position, Please place position X and Y in 0 ~ ${gridNumber -
           1}`
-      });
+      );
       return;
     }
 
-    if (this.getIndexFromArray(face, directions) === -1) {
+    if (getIndexFromArray(face, directions) === -1) {
       // ErrorMsg: invalid direction
-      this.setState({
-        message: `Invalid Direction, Please use NORTH, EAST, SOUTH, WEST`
-      });
+      setMessage("Invalid Direction, Please use NORTH, EAST, SOUTH, WEST");
       return;
     }
-
-    this.setState({
-      position: { x, y },
-      direction: face
-    });
+    setPosition({ x, y });
+    setDirection(face);
   };
 
   // move command
-  movePacman = () => {
-    const {
-      direction,
-      position: { x, y },
-      gridNumber
-    } = this.state;
-
-    const newX = x + directionMove[direction].moveX;
-    const newY = y + directionMove[direction].moveY;
+  const movePacman = () => {
+    const newX = position.x + directionMove[direction].moveX;
+    const newY = position.y + directionMove[direction].moveY;
 
     const newPosition = { x: newX, y: newY };
-    if (!this.availablePosition(newPosition)) {
+    if (!availablePosition(newPosition)) {
       // ErrorMsg: invalid move
-      this.setState({
-        message: `Invalid move, Please move position X and Y in 0 ~ ${gridNumber -
-          1}`
-      });
+      setMessage(
+        `Invalid move, Please move position X and Y in 0 ~ ${gridNumber - 1}`
+      );
       return;
     }
-
-    this.setState({
-      position: newPosition
-    });
+    setPosition(newPosition);
   };
 
   // left command or right command
-  rotatePacman = rotate => {
-    const { direction } = this.state;
-    const index = this.getIndexFromArray(direction, directions);
+  const rotatePacman = rotate => {
+    const index = getIndexFromArray(direction, directions);
 
     let newDirection;
     if (rotate === "LEFT") {
@@ -171,60 +148,44 @@ class Simulator extends React.Component {
     if (rotate === "RIGHT") {
       newDirection = directions[(index + 1) % 4];
     }
-
-    this.setState({
-      direction: newDirection
-    });
+    setDirection(newDirection);
   };
 
   // report command
-  report = () => {
-    this.setState({
-      isReport: true
-    });
+  const report = () => {
+    setIsReport(true);
   };
 
-  render() {
-    const {
-      gridNumber,
-      direction,
-      position,
-      isReport,
-      message,
-      inputCommand
-    } = this.state;
-
-    return (
-      <div>
-        <h1 className="simulator__title">Pacman</h1>
-        <div className="simulator__content">
-          <div className="simulator__control">
-            <InputArea
-              handleChange={this.handleChange}
-              onInputSubmit={this.onInputSubmit}
-              inputCommand={inputCommand}
-            />
-            <button onClick={() => this.handleClick()}>RESET</button>
-            {!!message ? (
-              <p className="simulator__message">Message: {message}</p>
-            ) : null}
-            {isReport ? (
-              <p className="simulator__output">
-                Output: {position.x},{position.y},{direction}
-              </p>
-            ) : null}
-          </div>
-          <div className="simulator__map">
-            <PacmanMap
-              gridNumber={gridNumber}
-              direction={direction}
-              position={position}
-            />
-          </div>
+  return (
+    <div>
+      <h1 className="simulator__title">Pacman</h1>
+      <div className="simulator__content">
+        <div className="simulator__control">
+          <InputArea
+            handleChange={handleChange}
+            onInputSubmit={onInputSubmit}
+            inputCommand={inputCommand}
+          />
+          <button onClick={handleClick}>RESET</button>
+          {!!message ? (
+            <p className="simulator__message">Message: {message}</p>
+          ) : null}
+          {isReport ? (
+            <p className="simulator__output">
+              Output: {position.x},{position.y},{direction}
+            </p>
+          ) : null}
+        </div>
+        <div className="simulator__map">
+          <PacmanMap
+            gridNumber={gridNumber}
+            direction={direction}
+            position={position}
+          />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Simulator;
